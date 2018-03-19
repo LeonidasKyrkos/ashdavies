a4c16681d47ffa09678f4e04073cbc1ee45e98f2
Ash Davies <ash.davies247@gmail.com>
2018-03-19 22:30:40 +0000

    almost no changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementsByClassName("nav_area")[0].style.width = "100%";
+    document.getElementsByClassName("nav")[0].style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementsByClassName("nav_area")[0].style.width = "0%";
+    document.getElementsByClassName("nav")[0].style.width = "0%";
 }
