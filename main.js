bcc88b75cde6aa0b58789c7eb0f8fe433b6f99fc
Ash Davies <ash.davies247@gmail.com>
2018-04-23 17:12:22 +0100

    message

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementsByClassName("nav")[0].style.width = "100%";
+    document.getElementsByClassName("nav")[0].style.transform = "translate(0)";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementsByClassName("nav")[0].style.width = "0%";
+    document.getElementsByClassName("nav")[0].style.transform = "translate(-100%)";
 }
