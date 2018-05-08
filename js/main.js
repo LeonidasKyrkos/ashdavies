94b9fa88c04ed7b9d3d50d8f0a37a779792a7243
Ash Davies <ash.davies247@gmail.com>
2018-05-08 20:22:14 +0100

    ever so slight changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-/* Open when someone clicks on the span element */
+/* the open and close of the nav */
 function openNav() {
     document.getElementsByClassName("nav")[0].style.transform = "translate(0)";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementsByClassName("nav")[0].style.transform = "translate(-100%)";
 }
